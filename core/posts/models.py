3a76631edd43363a60b920884ede6a9bfc9dd8e3
Lefevre <matthewrlefevre@gmail.com>
2019-05-13 14:01:14 -0600

    add slug to Post model

--- core/posts/models.py
+++ core/posts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.utils.text import slugify
+
 from core import settings
+
 
 # Create your models here.
 
@@ -17,8 +20,19 @@ class Post(models.Model):
     draft_started_datetime = models.DateTimeField(auto_now_add=True, editable=False)
     last_update = models.DateTimeField(auto_now=True, editable=False)
     visible = models.BooleanField(default=True)
-    
-    date_to_publish = models.DateTimeField(auto_now=False)
-    post_title = models.CharField(max_length=75)
+    publish_at = models.DateTimeField(auto_now=False)
+
+    post_title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
     post_contents = models.TextField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-publish_at')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.post_title,self.pk)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.post_title
