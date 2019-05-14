4809b4f560c881b0376dd4d96399ec6a3f1b2830
Lefevre <matthewrlefevre@gmail.com>
2019-05-13 22:38:48 -0600

    add post_form for use in creating and updating posts; add post/form.py

---
+++ core/posts/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from posts.models import Book, Post
+
+
+class PostForm(ModelForm):
+    model = Post
+    fields = [
+        'post_title',
+        'post_contents',
+    ]
+
+
+class BookForm(ModelForm):
+    model = Book
+    fields = [
+        'author',
+        'title',
+        'cover_image',
+        'isbn'
+    ]
