205d27e8af5d99f34532a04521ce9406e8abd337
Lefevre <matthewrlefevre@gmail.com>
2019-05-14 15:18:08 -0600

    update tests to include reverse urls

--- core/posts/tests.py
+++ core/posts/tests.py
@@ -1,29 +1,44 @@
 from django.test import Client, TestCase, SimpleTestCase
+from django.urls import reverse
 from users.models import CustomUser
 
 # Naming convention: test_{url_name, not actual url}_page
 
-class LoggedOutResponseTests(TestCase):
+class LoggedOutPostResponseTests(TestCase):
 
-    def test_accounts_page_status_code(self):
-        response = self.client.get("/accounts")
+    def test_login_page_status_code(self):
+        response = self.client.get("/accounts/login/")
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('account_login'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_signup_page_status_code(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('account_signup'))
         self.assertEqual(response.status_code, 200)
 
     def test_home_page_status_code(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
 
     def test_book_page_status_code(self):
-        response = self.client.get("/books")
+        response = self.client.get("/books/")
         self.assertEqual(response.status_code, 200)
 
     def test_new_post_page_status_code_logged_out(self):
-        response = self.client.get("/new_post")
-        self.assertEqual(response.status_code, 301)
+        response = self.client.get("/new_post/")
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('new_post'))
+        self.assertEqual(response.status_code, 302)
 
     def test_update_post_page_status_code_logged_out(self):
         response = self.client.get("/update_post")
         self.assertEqual(response.status_code, 301)
+        response = self.client.get(reverse("update_post"))
+        self.assertEqual(response.status_code, 302)
 
     # NOTE: Need to add tests for the admin page, and then perform admin hardening.
 
--- core/users/tests.py
+++ core/users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class LoggedOutUserResponseTests(TestCase):
+
+    def test_reviewers_page_status_code(self):
+        response = self.client.get("/reviewers/")
+        self.assertEqual(response.status_code, 200)
+    
