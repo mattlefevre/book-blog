65d1ebbe263c1b369f975e2d5d96ab04ac18e2cb
Lefevre <matthewrlefevre@gmail.com>
2019-05-14 13:02:21 -0600

    update tests to accurately log in test user

--- core/posts/tests.py
+++ core/posts/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase, SimpleTestCase
+from users.models import CustomUser
 
 # Create your tests here.
-class SimpleTests(SimpleTestCase):
+class LoggedOutResponseTests(TestCase):
     # Naming convention: test_{url_name}_page
 
     def test_accounts_page_status_code(self):
@@ -12,12 +13,42 @@ class SimpleTests(SimpleTestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_new_post_page_status_code(self):
+    def test_new_post_page_status_code_logged_out(self):
+        response = self.client.get("/new_post")
+        self.assertEqual(response.status_code, 301)
+
+    def test_update_post_page_status_code_logged_out(self):
+        response = self.client.get("/update_post")
+        self.assertEqual(response.status_code, 301)
+
+    # NOTE: Need to add tests for the admin page, and then perform admin hardening.
+
+class LoggedInResponseTests(TestCase):
+    def set_up(self):
+        self.credentials = {
+            'email_address':'testuser@books-and-bones.com',
+            'password':'a-secret-pw1!',
+            'username':'testuername'
+        }
+        CustomUser.objects.create_user(**self.credentials)
+    
+    def test_login(self):
+        self.set_up()
+        #login
+        response = self.client.post('accounts/login/', 
+            **email=self.credentials['email_address'],
+            **password=self.credentials['password'],follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['CustomUser'].is_authenticated)
+    
+    def test_new_post_page_status_code_logged_in(self):
+        self.set_up()
+        response = self.client.post('accounts/login/', **self.credentials, follow=True)
         response = self.client.get("/new_post")
         self.assertEqual(response.status_code, 200)
 
-    def test_update_post_page_status_code(self):
+    def test_update_post_page_status_code_logged_in(self):
+        self.set_up()
+        response = self.client.post('accounts/login/', **self.credentials, follow=True)
         response = self.client.get("/update_post")
         self.assertEqual(response.status_code, 200)
-
-    # NOTE: Need to add tests for the admin page, and then perform admin hardening.
