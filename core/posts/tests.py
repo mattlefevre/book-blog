a7b9a9292ca1cb29b4836e11cd59cd3994410d21
Lefevre <matthewrlefevre@gmail.com>
2019-05-14 12:26:39 -0600

    add tests to verify 200 for all current pages' status codes.

--- core/posts/tests.py
+++ core/posts/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
+class SimpleTests(SimpleTestCase):
+    # Naming convention: test_{url_name}_page
+
+    def test_accounts_page_status_code(self):
+        response = self.client.get("/accounts")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_post_page_status_code(self):
+        response = self.client.get("/new_post")
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_post_page_status_code(self):
+        response = self.client.get("/update_post")
+        self.assertEqual(response.status_code, 200)
+
+    # NOTE: Need to add tests for the admin page, and then perform admin hardening.
