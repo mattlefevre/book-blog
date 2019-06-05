e1c00d987b22e9144ead58df2c2df541f580b9dc
mattlefevre <matthewrlefevre@gmail.com>
2019-06-04 18:28:31 -0600

    update j/s to remove typed-inbook information if the 'not about a book' box is checked

--- core/static/posts/post_form.js
+++ core/static/posts/post_form.js
@@ -1,9 +1,14 @@
 var checkbox = document.querySelector("input[name=book_check]");
 var book_form = document.getElementById("include_book")
+var book_lis = document.getElementById("include_book").getElementsByTagName("input")
 
 checkbox.addEventListener( 'change', function() {
     if(this.checked) {
         book_form.className = "checked"
+        // only looks at book's title and author, ignoring all other fields since they'll be deleted on submit.
+        for (let index = 0; index < 2; index++) {
+            book_lis[index].value = ""          
+        }
     } else {
         book_form.className = "form-group"
     }
