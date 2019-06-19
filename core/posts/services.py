a6ea38e162a3db34a4da35ad3e5385c4e6738ed9
mattlefevre <matthewrlefevre@gmail.com>
2019-06-18 18:20:17 -0600

    update create_post_and_book to account for updated image location

--- core/posts/services.py
+++ core/posts/services.py
@@ -33,32 +33,35 @@ def create_post_and_book(*, form: "PostAndBookForm", user: CustomUser) -> None:
         poster = user
         post_title = form.cleaned_data["post_title"]
         post_contents = form.cleaned_data["post_contents"]
-
-        # book_check: Boolean field that is true when checked.
-        # "True" is equivelant to "No book"
+        image = form.cleaned_data["image"]
 
         author = form.cleaned_data["author"]
         title = form.cleaned_data["title"]
         synopsis = form.cleaned_data["synopsis"]
-        cover_image = form.cleaned_data["cover_image"]
         isbn = form.cleaned_data["isbn"]
         rating = form.cleaned_data["rating"]
-        if not no_book_check(form=form.cleaned_data):
+
+        if no_book_check(form=form.cleaned_data):
+            post_to_db = Post(post_title=post_title, post_contents=post_contents)
+        else:
             # Save with book
             book_to_db = Book(
                 author=author,
                 title=title,
                 synopsis=synopsis,
-                cover_image=cover_image,
                 isbn=isbn,
                 rating=rating,
             )
             book_to_db.save()
-            post_to_db = Post(
-                post_title=post_title, post_contents=post_contents, book=book_to_db
-            )
-        else:
-            post_to_db = Post(post_title=post_title, post_contents=post_contents)
+            post_to_db = Post(post_title=post_title, post_contents=post_contents, book=book_to_db, image=image) 
+        
         post_to_db.poster = user
         post_to_db.save()
     return forms.ValidationError("Something went wrong with create_and_post_book")
+
+def book_already_exists(author, title):
+    #NOTE: Not quite done here.
+    try:
+        existing_book = Book.objects.get(author=author, title=title)
+    finally:
+        print(existing_book)
