eaa110d62cd497446ba4a6f055acd7a68e73c050
mattlefevre <matthewrlefevre@gmail.com>
2019-06-18 18:01:30 -0600

    remove secret key from public repository

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "5aoupyjr9)*olknrgbp1)j7!-5gjw+96+-14#6w2(ofgo$#ip0"
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
